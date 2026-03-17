9033786d90e8e99f9c6b041b4dc447e11f1d5ba7
Yash Thakkar <EMAIhuntersthecode@gmail.com>
2026-03-17 13:54:18 +0530

    clean

--- components/NihalLogo.ts
+++
@@ -1,2 +0,0 @@
-// Re-export the logo for easier imports
-export { default as NihalLogo } from '../dist/assets/nihal logo .png';
--- types/png.d.ts
+++
@@ -1,7 +0,0 @@
-// TypeScript module declaration for importing PNG files
-// Allows importing PNGs as modules in TS/TSX files
-
-declare module '*.png' {
-  const value: string;
-  export default value;
-}
